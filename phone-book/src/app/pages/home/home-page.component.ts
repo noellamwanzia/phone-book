ffca586a6ee33503346f50a798b99626d6650536
noellamwanzia <noellamwanzia@gmail.com>
2023-02-17 12:50:09 +0300

    🐛use correct selector on template.

--- phone-book/src/app/pages/home/home-page.component.ts
+++ phone-book/src/app/pages/home/home-page.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss']
 })

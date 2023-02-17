e483bf25bc95739f8c352005de7fa47d97e1662f
noellamwanzia <noellamwanzia@gmail.com>
2023-02-17 12:39:50 +0300

    🔧add routing

--- phone-book/src/app/app-routing.module.ts
+++ phone-book/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ContactFormComponent } from './components/contact-form/contact-form.component';
+import { HomePageComponent } from './pages/home/home-page.component';
+
+const routes: Routes = [
+  {path:'', component: HomePageComponent},
+  {path:'create-contact', component: ContactFormComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

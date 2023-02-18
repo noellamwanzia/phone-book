47a172b960c09ef0da1784f99d6be23ddf62e7ed
noellamwanzia <noellamwanzia@gmail.com>
2023-02-18 18:40:44 +0300

    🔧add template logic to mat table

--- phone-book/src/app/components/contact-table/contact-table.component.ts
+++ phone-book/src/app/components/contact-table/contact-table.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import {MatTableDataSource} from '@angular/material/table';
+import { Router } from '@angular/router';
+import { Contact } from 'src/app/models/contact.interface';
+
+import { ContactService } from 'src/app/services/contact.service';
 
 @Component({
   selector: 'app-contact-table',
@@ -6,5 +12,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact-table.component.scss']
 })
 
-export class ContactTableComponent {
+export class ContactTableComponent implements OnInit 
+{
+
+  displayedColumns: string[] = ['name', 'actions'];
+
+  dataSource: MatTableDataSource<Contact> = new MatTableDataSource();
+
+  constructor(private _contactService: ContactService,
+              private _router: Router){}
+
+  ngOnInit(): void {
+    const contacts$ = this._contactService.getContacts();
+
+    //set mat table data
+    contacts$.subscribe(contacts => this.dataSource.data = contacts);
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  deleteContact(contactId: string)
+  {
+    this._contactService.deleteContact(contactId)
+                          .then(res => console.log('Contact successfully deleted.'))
+                          .catch(res => console.log('Ooops, something went wrong'))
+  }
+
+  addContact()
+  {
+    this._router.navigate(['contact-form'])
+  }
+
 }
